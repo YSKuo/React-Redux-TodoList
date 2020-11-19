c57146d1a0dc1ca277406d1ba2ba0390a92a41c0
YSkuo <crowley3141@hotmail.com>
2020-11-19 13:42:03 +0800

    fix addTodo: add todo only when content has value

--- src/redux/reducers/todos.js
+++ src/redux/reducers/todos.js
@@ -7,13 +7,16 @@ const initialState = {
 export default function todosReducer(state = initialState, action) {
   switch(action.type) {
     case ADD_TODO: {
-      return {
-        ...state,
-        todos: [...state.todos, {
-          id: Date.now(),
-          content: action.payload.content,
-          isCompleted: false,
-        }]
+      const content = action.payload.content.trim();
+      if (content) {
+        return {
+          ...state,
+          todos: [...state.todos, {
+            id: Date.now(),
+            content,
+            isCompleted: false,
+          }]
+        }
       }
     }
     case DELETE_TODO: {

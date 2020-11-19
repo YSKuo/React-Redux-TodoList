ada999c9be80e4773dddb4c5b9489e0fcc0e44b0
YSkuo <crowley3141@hotmail.com>
2020-11-19 16:48:12 +0800

    remove unrelated folder

--- src-fe301/redux/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const ADD_TODO = 'add_todo';
-export const DELETE_TODO = 'delete_todo';
-
-export const ADD_USER = 'add_user';
--- src-fe301/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import todos from './todos';
-import users from './users';
-
-// 把所有 reducer 引進來合體
-export default combineReducers({ 
-  todoState: todos, 
-  users 
-});
